9313412f8ad85419225c51e9a8793fe1eb21a225
Oksana Paduchak <2992paduhcak80@gmail.com>
2024-12-17 00:47:09 +0200

    js homework-3

---
+++ js/task-2.js
@@ -0,0 +1,22 @@
+function makeArray(firstArray, secondArray, maxLength) {
+
+    let unitedArray = firstArray.concat(secondArray);
+    const newArray = [];
+
+    for (i = 0; i < unitedArray.length; i++) {
+        if (i < maxLength) {
+            newArray.push(unitedArray[i]);
+        }
+    }
+    return newArray;
+}
+
+
+    
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
